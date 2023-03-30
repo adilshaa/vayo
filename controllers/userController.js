de814ff9845e8e61845ab9e8ba1eecc072638f24
Adilsha <adilsha042@gmail.com>
2023-03-30 12:23:37 +0530

    2RAERAG

--- controllers/userController.js
+++ controllers/userController.js
@@ -442,7 +442,7 @@ const LoadUserpage = async (req, res) => {
 
       const userData = await User.findOne({ _id: req.session.user._id });
       console.log(userData);
-      res.render("Userpage", {
+      res.render("userpage", {
         userData: userData,
         PassUserName: UserNameData,
       });
